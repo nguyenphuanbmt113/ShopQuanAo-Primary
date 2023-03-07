c2164b49a6763d16d5e11d982a02da14593e70ef
nguyenphuanbmt113 <nguyenphuanbmt113@gmail.com>
2023-03-07 22:16:31 +0700

    protect route & account page

--- client/src/page/screen/Home/Home.js
+++ client/src/page/screen/Home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { LoginHeader } from "../../../components/LoginHeader/LoginHeader";
+import { NavHome } from "../../../components/NavHome/NavHome";
 
 export const Home = () => {
   return (
     <div>
-      <LoginHeader></LoginHeader>
+      <NavHome></NavHome>
+      <div className="text-cmm">Nguyen Phu An</div>
     </div>
   );
 };
